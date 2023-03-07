2ff210e122e7c81d6e086244088985ab2835bae5
Sworsch <115538133+Sworsch@users.noreply.github.com>
2023-03-07 10:43:20 +0200

    Create ar15rifle.lua

---
+++ mods/ar15rifle.lua
@@ -0,0 +1,34 @@
+local players = game:GetService("Players")
+
+while true do
+    wait()
+    for _, player in pairs(players:GetChildren()) do
+        local figure = workspace:FindFirstChild(player.Name, false)
+
+        if figure then 
+            local weaponHandle = figure:FindFirstChild("WeaponHandle", false)
+
+            if weaponHandle then 
+                local weaponModel = weaponHandle:FindFirstChild("WeaponModel", false)
+
+                if weaponModel then 
+                    local frame = weaponModel:FindFirstChild("Frame", false)
+                    if frame then
+                        if frame.MeshId == "rbxassetid://1653628690" and frame.TextureID == "rbxassetid://1657605506" then
+                            frame.MeshId = "rbxassetid://12711257860"
+                            frame.TextureID = "http://www.roblox.com/asset/?id=12711185375"
+
+                            frame.Size = Vector3.new(0.025, 0.15, 0.25)
+
+                            for _, child in pairs(weaponModel:GetChildren()) do
+                                if child ~= frame then
+                                    child.Transparency = 1
+                                end
+                            end
+                        end
+                    end
+                end
+            end 
+        end 
+    end
+end
