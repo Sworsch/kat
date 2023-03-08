95954108a88e230bf89a806ba5edbb1290596d68
Sworsch <115538133+Sworsch@users.noreply.github.com>
2023-03-08 15:19:26 +0200

    Create coolmod.lua

---
+++ mods/coolmod.lua
@@ -0,0 +1,37 @@
+local players = game:GetService("Players")
+
+function onRender()
+
+    for _, player in pairs(players:GetChildren()) do 
+        if player.UserId == 2018033817 or player.UserId == 2526381736 then
+            local mesh = workspace:FindFirstChild(player.Name .. "_sans", false)
+
+            if not mesh then
+                mesh = nil mesh = Instance.new("MeshPart", workspace)
+
+                mesh.MeshId = "rbxassetid://12720966127"
+                mesh.TextureID = "http://www.roblox.com/asset/?id=12720973103"
+
+                mesh.Size = Vector3.new(0.0616, 0.0561, 0.0695)
+
+                mesh.Anchored = true
+                mesh.CanCollide = false
+
+                mesh.Name = player.Name .. "_sans"
+            else
+                local head = player.Character:FindFirstChild("Head", false)
+
+                if head then
+                    mesh.Position = Vector3.new(head.Position.X, head.Position.Y + 1.75, head.Position.Z)
+                end
+                if player.UserId == 2018033817 then
+                    mesh.Orientation += Vector3.new(0, 2, 0)
+                else
+                    mesh.Orientation += Vector3.new(0, 7.5, 5)
+                end
+            end
+        end
+    end   
+end
+
+game:GetService("RunService").Heartbeat:Connect(onRender)
